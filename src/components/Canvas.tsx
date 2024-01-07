2fe1f394c63a393f9c5235ccf06c2be79df7b5aa
Michael Showes <michael.showes@gmail.com>
2024-01-07 05:14:49 -0500

    moved logos to separate file

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { useCanvasContext } from '@/context/canvasContext';
-import Image from 'next/image';
-import { CSS } from '@dnd-kit/utilities';
 import {
 	DndContext,
 	DragEndEvent,
@@ -12,59 +9,13 @@ import {
 	useSensors
 } from '@dnd-kit/core';
 import {
-	arrayMove,
 	SortableContext,
-	rectSwappingStrategy,
-	useSortable
+	arrayMove,
+	rectSwappingStrategy
 } from '@dnd-kit/sortable';
 
-import { LogoProps } from '@/types';
-
-function SortableLogo({ image }: { image: LogoProps }) {
-	const { attributes, listeners, setNodeRef, transform, transition } =
-		useSortable({ id: image.id });
-	const { setCanvasImages } = useCanvasContext();
-
-	const removeLogoFromCanvas = (image: LogoProps) => {
-		image.isOnCanvas = false;
-		setCanvasImages((prevImages) =>
-			prevImages.filter((prevImage) => prevImage.id !== image.id)
-		);
-	};
-
-	const style = {
-		transition,
-		transform: CSS.Transform.toString(transform)
-	};
-
-	return (
-		<div
-			ref={setNodeRef}
-			{...attributes}
-			{...listeners}
-			style={style}
-			className={
-				'group relative overflow-hidden before:cursor-move before:absolute before:content-[""] before:inset-0 before:opacity-0 before:bg-orange-400 before:mix-blend-overlay before:transition before:duration-300 hover:before:opacity-100 ease-in-out'
-			}
-			id={image.name}
-		>
-			<Image
-				src={image.url || ''}
-				width={344}
-				height={206}
-				alt={image.name}
-			/>
-			<button
-				onClick={() => removeLogoFromCanvas(image)}
-				className={
-					'group-hover:-translate-y-6 transition-transform duration-300 ease-in-out absolute right-0 -bottom-6 text-white bg-red-500 font-semibold text-xs uppercase h-6 p-1'
-				}
-			>
-				Remove
-			</button>
-		</div>
-	);
-}
+import { useCanvasContext } from '@/context/canvasContext';
+import SortableLogo from '@/components/SortableLogo';
 
 export default function Canvas() {
 	const { canvasImages, setCanvasImages, ref } = useCanvasContext();
--- src/components/SortableLogo.tsx
+++ src/components/SortableLogo.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import { CSS } from '@dnd-kit/utilities';
+import { useSortable } from '@dnd-kit/sortable';
 
 import { useCanvasContext } from '@/context/canvasContext';
-import { LogoProps } from '@/lib/figma';
-import { useSortable } from '@dnd-kit/sortable';
+import type { LogoProps } from '@/types';
 
 export default function SortableLogo({ image }: { image: LogoProps }) {
 	const { attributes, listeners, setNodeRef, transform, transition } =
@@ -28,10 +28,9 @@ export default function SortableLogo({ image }: { image: LogoProps }) {
 			{...attributes}
 			{...listeners}
 			style={style}
-			className={`
-        relative overflow-hidden,
-        before:cursor-move
-      `}
+			className={
+				'group relative overflow-hidden before:cursor-move before:absolute before:content-[""] before:inset-0 before:opacity-0 before:bg-orange-400 before:mix-blend-overlay before:transition before:duration-300 hover:before:opacity-100 ease-in-out'
+			}
 			id={image.name}
 		>
 			<Image
@@ -40,7 +39,14 @@ export default function SortableLogo({ image }: { image: LogoProps }) {
 				height={206}
 				alt={image.name}
 			/>
-			<button onClick={() => removeLogoFromCanvas(image)}>Remove</button>
+			<button
+				onClick={() => removeLogoFromCanvas(image)}
+				className={
+					'group-hover:-translate-y-6 transition-transform duration-300 ease-in-out absolute right-0 -bottom-6 text-white bg-red-500 font-semibold text-xs uppercase h-6 p-1'
+				}
+			>
+				Remove
+			</button>
 		</div>
 	);
 }
